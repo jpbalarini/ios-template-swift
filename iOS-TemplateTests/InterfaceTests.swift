27814b4cda70c3cb236535451ed529e61383cb9a
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-10-26 17:03:14 -0300

    Add failing test

--- iOS-TemplateTests/InterfaceTests.swift
+++ iOS-TemplateTests/InterfaceTests.swift
@@ -25,7 +25,7 @@ class InterfaceTests: KIFTestCase {
     }
     
     func testExample() {
-        tester().waitForViewWithAccessibilityLabel("Hello World!")
+        tester().waitForViewWithAccessibilityLabel("Hello Worsssld!")
         tester().tapViewWithAccessibilityLabel("Tap to show alert")
         tester().tapViewWithAccessibilityLabel("OK")
         //tester().waitForTimeInterval(1)
