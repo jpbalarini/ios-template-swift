40a628ffc07098b078e7029fcb027c515ebce9b9
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-11-05 21:57:37 -0300

    Testing with xctool

--- iOS-Template/Scripts/build.sh
+++ iOS-Template/Scripts/build.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xcodebuild -workspace iOS-Template.xcworkspace -scheme Production -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty -c && exit ${PIPESTATUS[0]}
+
+xctool -workspace iOS-Template.xcworkspace -scheme 'Production' -configuration Release -sdk iphoneos7.1 CONFIGURATION_BUILD_DIR='~/build/' build
