38587b4060a98b77ff76ea43f22754d598301ba9
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-11-10 13:52:35 -0300

    Revert to old conf

--- iOS-Template/Scripts/build.sh
+++ iOS-Template/Scripts/build.sh
@@ -2,4 +2,4 @@
 # xcodebuild -workspace iOS-Template.xcworkspace -scheme Production -sdk iphoneos -configuration Release OBJROOT=$PWD/build SYMROOT=$PWD/build ONLY_ACTIVE_ARCH=NO CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty -c && exit ${PIPESTATUS[0]}
 
 # xctool -workspace iOS-Template.xcworkspace -scheme 'Production' -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR='~/build/' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO build
-xctool -workspace iOS-Template.xcworkspace -scheme 'Production' -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR='~/build/' 'CODE_SIGN_RESOURCE_RULES_PATH=$(SDKROOT)/ResourceRules.plist' build
+xctool -workspace iOS-Template.xcworkspace -scheme 'Production' -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR='~/build/' CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO build
