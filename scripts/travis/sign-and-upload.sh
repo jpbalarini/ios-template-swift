956eee8acd885a1e6925a57f6d553f72a08dbc01
Juan Pablo Balarini <jpbalarini@gmail.com>
2015-11-13 21:17:11 -0300

    Do deploy only on develop

--- scripts/travis/sign-and-upload.sh
+++ scripts/travis/sign-and-upload.sh
@@ -3,9 +3,9 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   echo "This is a pull request. No deployment will be done."
   exit 0
 fi
-# Only deploy master
-if [[ "$TRAVIS_BRANCH" != "master" ]]; then
-  echo "Testing on a branch other than master. No deployment will be done."
+# Only deploy develop
+if [[ "$TRAVIS_BRANCH" != "develop" ]]; then
+  echo "Testing on a branch other than develop. No deployment will be done."
   exit 0
 fi
 
